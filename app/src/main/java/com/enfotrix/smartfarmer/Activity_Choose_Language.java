965b0fee4e05ed4229eef4d8a05d572e98ad63ee
JUNAID ANWAR <khan114384@gmail.com>
2021-11-30 09:50:34 +0500

    Screen of choose your language completed

--- app/src/main/java/com/enfotrix/smartfarmer/Activity_Choose_Language.java
+++ app/src/main/java/com/enfotrix/smartfarmer/Activity_Choose_Language.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignupActivity extends AppCompatActivity {
+public class Activity_Choose_Language extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
